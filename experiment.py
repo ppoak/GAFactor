0858539c62f636c24b58e2911dc4b49236be9890
oak <oakery@qq.com>
2022-10-17 10:48:45 +0800

    FEAT: Add caculation by numpy

--- experiment.py
+++ experiment.py
@@ -1,6 +1,6 @@
 # %% 
-
 import time
+import numba
 import random
 import itertools
 import operators
@@ -28,6 +28,12 @@ adj_high_train = adj_high.loc[train_start:train_end]
 adj_low_train = adj_low.loc[train_start:train_end]
 volume_train = volume.loc[train_start:train_end]
 label_train = label.loc[train_start:train_end]
+adj_open_test = adj_open.loc[test_start:test_end]
+adj_close_test = adj_close.loc[test_start:test_end]
+adj_high_test = adj_high.loc[test_start:test_end]
+adj_low_test = adj_low.loc[test_start:test_end]
+volume_test = volume.loc[test_start:test_end]
+label_test = label.loc[test_start:test_end]
 
 # %%
 pset = gp.PrimitiveSetTyped("MAIN", itertools.repeat(pd.DataFrame, 5), pd.DataFrame, "ARG")
@@ -61,13 +67,24 @@ def pearson_corr(x, y):
 
 def evaluate(individual):
     func = toolbox.compile(individual)
-    pred = func(adj_open, adj_high, adj_low, adj_close, volume)
+    pred = func(adj_open_train, adj_high_train, adj_low_train, adj_close_train, volume_train)
     if not isinstance(pred, pd.DataFrame):
         return np.nan,
-    corrs = pred.corrwith(label, axis=1)
+    corrs = pred.corrwith(label_train, axis=1)
     return np.abs(corrs.mean()),
 
-toolbox.register("evaluate", evaluate)
+def evaluate_np(individual):
+    func = toolbox.compile(individual)
+    pred = func(adj_open_train, adj_high_train, adj_low_train, adj_close_train, volume_train).values
+    if not isinstance(pred, np.ndarray):
+        return np.nan
+    pred_demean = np.nan_to_num(pred - np.nanmean(pred, axis=1).repeat(pred.shape[1]).reshape(pred.shape[0], pred.shape[1]))
+    label_demean = np.nan_to_num(label_train.values - np.nanmean(label_train.values, axis=1).repeat(
+        label_train.values.shape[1]).reshape(label_train.values.shape[0], label_train.values.shape[1]))
+    corrs = (pred_demean @ label_demean.T).diagonal() / (np.linalg.norm(pred_demean) * np.linalg.norm(label_demean))
+    return np.abs(corrs.mean()),
+
+toolbox.register("evaluate", evaluate_np)
 toolbox.register("select", tools.selTournament, tournsize=3)
 toolbox.register("mate", gp.cxOnePoint)
 toolbox.register("expr_mut", gp.genFull, min_=0, max_=2)
@@ -75,7 +92,7 @@ toolbox.register("mutate", gp.mutUniform, expr=toolbox.expr_mut, pset=pset)
 
 # %%
 def async_map(func, jobs):
-    return Parallel(n_jobs=-1, backend='threading')(delayed(func)(job) for job in jobs)
+    return Parallel(n_jobs=1, backend='threading')(delayed(func)(job) for job in jobs)
 toolbox.register("map", async_map)
 
 # %%
@@ -83,7 +100,7 @@ def main():
     random.seed(10)
     start_time = time.time()
     pop = toolbox.population(n=100)
-    hof = tools.HallOfFame(10)
+    hof = tools.HallOfFame(1)
     stats = tools.Statistics(lambda ind: ind.fitness.values)
     stats.register("elapsed", lambda _: f"{time.time() - start_time:.2f}s")
     stats.register("avg", np.nanmean)
@@ -91,10 +108,13 @@ def main():
     stats.register("min", np.nanmin)
     stats.register("max", np.nanmax)
 
-    algorithms.eaSimple(pop, toolbox, 0.5, 0.2, 40, stats, halloffame=hof, verbose=True)
+    algorithms.eaSimple(pop, toolbox, 0.5, 0.2, 2, stats, halloffame=hof, verbose=True)
 
     return pop, stats, hof
 
 _, _, hof = main()
 for elite in hof:
     print(elite)
+    pred_test = toolbox.compile(elite)(adj_open_test, adj_high_test, adj_low_test, adj_close_test, volume)
+    corr = pred_test.corrwith(label, axis=1)
+    print(f"Noraml IC on test Dataset: {np.abs(corr.mean())}")
